b069a1a36b92459a568c80f3f0ec39d1caf7a03c
2125697 <LE2125697@crc-lennox.qc.ca>
2022-11-21 00:01:41 -0500

    Insert data to ScreenRoom table

---
+++ dbo.ScreenRoom(2).data.sql
@@ -0,0 +1,5 @@
+﻿SET IDENTITY_INSERT [dbo].[ScreenRoom] ON
+INSERT INTO [dbo].[ScreenRoom] ([ScreenRoomId], [MovieId], [RoomNo], [TotalSeatNo]) VALUES (1, 1, 2, 5)
+INSERT INTO [dbo].[ScreenRoom] ([ScreenRoomId], [MovieId], [RoomNo], [TotalSeatNo]) VALUES (2, 2, 3, 5)
+INSERT INTO [dbo].[ScreenRoom] ([ScreenRoomId], [MovieId], [RoomNo], [TotalSeatNo]) VALUES (3, 3, 1, 5)
+SET IDENTITY_INSERT [dbo].[ScreenRoom] OFF
---
+++ dbo.ScreenRoom.data.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO [dbo].[ScreenRoom] ([ScreenRoomId], [MovieName], [RoomNo], [TotalSeatNo]) VALUES (CAST(1.0000 AS Money), N'The Matrix', 2, 5)
+INSERT INTO [dbo].[ScreenRoom] ([ScreenRoomId], [MovieName], [RoomNo], [TotalSeatNo]) VALUES (CAST(2.0000 AS Money), N'Titanic', 3, 5)
+INSERT INTO [dbo].[ScreenRoom] ([ScreenRoomId], [MovieName], [RoomNo], [TotalSeatNo]) VALUES (CAST(3.0000 AS Money), N'Top Gun', 1, 5)

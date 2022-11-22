fea3bd2f508ef7a0056cceff0dec49164f22970e
2125697 <LE2125697@crc-lennox.qc.ca>
2022-11-22 11:24:52 -0500

    Create movie table

---
+++ Create dbo.Movies_1.sql
@@ -0,0 +1,19 @@
+﻿USE [C:\OOP2\OOP2PROJECT\MOVIEDATABASEFINAL\MOVIEDATABASEFINAL\MOVIETHEATRE.MDF]
+GO
+
+/****** Object: Table [dbo].[Movies] Script Date: 11/22/2022 11:20:18 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Movies] (
+    [MovieId]       INT          IDENTITY (1, 1) NOT NULL,
+    [MovieName]     VARCHAR (50) NOT NULL,
+    [Genre]         VARCHAR (50) NOT NULL,
+    [DateOfRelease] INT          NOT NULL,
+    [Actors]        VARCHAR (50) NOT NULL
+);
+
+

d00658a059457004ce43f4375c78c96d68b5ef00
2125697 <LE2125697@crc-lennox.qc.ca>
2022-11-22 11:27:22 -0500

    Insert data into MovieTicketUser

---
+++ dbo.MovieTicketUser.data.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO [dbo].[MovieTicketUser] ([MovieId], [TicketId], [UserId]) VALUES (1, 123432, 2)
+INSERT INTO [dbo].[MovieTicketUser] ([MovieId], [TicketId], [UserId]) VALUES (2, 756786, 1)
+INSERT INTO [dbo].[MovieTicketUser] ([MovieId], [TicketId], [UserId]) VALUES (2, 987643, 3)
+INSERT INTO [dbo].[MovieTicketUser] ([MovieId], [TicketId], [UserId]) VALUES (3, 454686, 2)

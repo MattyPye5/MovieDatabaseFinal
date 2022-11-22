295bbfd127fcc50cf29145a9aba059bec47ccdc9
2125697 <LE2125697@crc-lennox.qc.ca>
2022-11-22 11:19:03 -0500

    Insert data into movies table

---
+++ dbo.Movies.data.sql
@@ -0,0 +1,5 @@
+﻿SET IDENTITY_INSERT [dbo].[Movies] ON
+INSERT INTO [dbo].[Movies] ([MovieId], [MovieName], [Genre], [DateOfRelease], [Actors]) VALUES (1, N'The Matrix', N'Action', 1999, N'Keenu Reeves')
+INSERT INTO [dbo].[Movies] ([MovieId], [MovieName], [Genre], [DateOfRelease], [Actors]) VALUES (2, N'Titanic', N'Drama', 1997, N'Kate Winslet, Leonardo DiCaprio')
+INSERT INTO [dbo].[Movies] ([MovieId], [MovieName], [Genre], [DateOfRelease], [Actors]) VALUES (3, N'Top Gun', N'Action', 1986, N'Tom Cruise, Kelly McGills')
+SET IDENTITY_INSERT [dbo].[Movies] OFF

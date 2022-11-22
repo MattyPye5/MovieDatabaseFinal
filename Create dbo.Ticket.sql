411a7d98a3e923971a5f7602f9d70bcbd1d3d545
2125697 <LE2125697@crc-lennox.qc.ca>
2022-11-22 11:54:34 -0500

    Create Create Ticket

---
+++ Create dbo.Ticket.sql
@@ -0,0 +1,17 @@
+﻿USE [C:\OOP2\OOP2PROJECT\MOVIEDATABASEFINAL\MOVIEDATABASEFINAL\MOVIETHEATRE.MDF]
+GO
+
+/****** Object: Table [dbo].[Ticket] Script Date: 11/22/2022 11:53:57 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Ticket] (
+    [ticketId]     INT          IDENTITY (1, 1) NOT NULL,
+    [E_ticket]     INT          NOT NULL,
+    [availability] VARCHAR (50) NOT NULL
+);
+
+
---
+++ dbo.Ticket.data.sql
@@ -0,0 +1,7 @@
+﻿SET IDENTITY_INSERT [dbo].[Ticket] ON
+INSERT INTO [dbo].[Ticket] ([ticketId], [E_ticket], [availability]) VALUES (1, 42653, N'available')
+INSERT INTO [dbo].[Ticket] ([ticketId], [E_ticket], [availability]) VALUES (2, 75757, N'sold')
+INSERT INTO [dbo].[Ticket] ([ticketId], [E_ticket], [availability]) VALUES (3, 8087, N'available')
+INSERT INTO [dbo].[Ticket] ([ticketId], [E_ticket], [availability]) VALUES (4, 83675, N'sold')
+INSERT INTO [dbo].[Ticket] ([ticketId], [E_ticket], [availability]) VALUES (5, 52644, N'sold')
+SET IDENTITY_INSERT [dbo].[Ticket] OFF

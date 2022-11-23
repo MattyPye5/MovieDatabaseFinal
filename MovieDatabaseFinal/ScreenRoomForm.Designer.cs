9bf2390dac00f42829cd1997ecc04756b42f8975
2125697 <LE2125697@crc-lennox.qc.ca>
2022-11-23 09:18:44 -0500

    Create screenRoomForm

---
+++ MovieDatabaseFinal/ScreenRoomForm.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace MovieDatabaseFinal
+{
+    partial class ScreenRoomForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ScreenRoomForm));
+            this.movieTheatreDataSet = new MovieDatabaseFinal.MovieTheatreDataSet();
+            this.screenRoomBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.screenRoomTableAdapter = new MovieDatabaseFinal.MovieTheatreDataSetTableAdapters.ScreenRoomTableAdapter();
+            this.tableAdapterManager = new MovieDatabaseFinal.MovieTheatreDataSetTableAdapters.TableAdapterManager();
+            this.screenRoomBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.screenRoomBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.screenRoomDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.movieTheatreDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.screenRoomBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.screenRoomBindingNavigator)).BeginInit();
+            this.screenRoomBindingNavigator.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.screenRoomDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // movieTheatreDataSet
+            // 
+            this.movieTheatreDataSet.DataSetName = "MovieTheatreDataSet";
+            this.movieTheatreDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // screenRoomBindingSource
+            // 
+            this.screenRoomBindingSource.DataMember = "ScreenRoom";
+            this.screenRoomBindingSource.DataSource = this.movieTheatreDataSet;
+            // 
+            // screenRoomTableAdapter
+            // 
+            this.screenRoomTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.MoviesTableAdapter = null;
+            this.tableAdapterManager.MovieTicketUserTableAdapter = null;
+            this.tableAdapterManager.ScreenRoomTableAdapter = this.screenRoomTableAdapter;
+            this.tableAdapterManager.ShowTimeTableAdapter = null;
+            this.tableAdapterManager.TicketTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = MovieDatabaseFinal.MovieTheatreDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            this.tableAdapterManager.UserDataTableAdapter = null;
+            // 
+            // screenRoomBindingNavigator
+            // 
+            this.screenRoomBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.screenRoomBindingNavigator.BindingSource = this.screenRoomBindingSource;
+            this.screenRoomBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.screenRoomBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.screenRoomBindingNavigator.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.screenRoomBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem,
+            this.screenRoomBindingNavigatorSaveItem});
+            this.screenRoomBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.screenRoomBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.screenRoomBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.screenRoomBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.screenRoomBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.screenRoomBindingNavigator.Name = "screenRoomBindingNavigator";
+            this.screenRoomBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.screenRoomBindingNavigator.Size = new System.Drawing.Size(800, 27);
+            this.screenRoomBindingNavigator.TabIndex = 0;
+            this.screenRoomBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(29, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "Move first";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(29, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "Move previous";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Position";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 27);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Current position";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(45, 20);
+            this.bindingNavigatorCountItem.Text = "of {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorMoveNextItem.Text = "Move next";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorMoveLastItem.Text = "Move last";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorAddNewItem.Text = "Add new";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorDeleteItem.Text = "Delete";
+            // 
+            // screenRoomBindingNavigatorSaveItem
+            // 
+            this.screenRoomBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.screenRoomBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("screenRoomBindingNavigatorSaveItem.Image")));
+            this.screenRoomBindingNavigatorSaveItem.Name = "screenRoomBindingNavigatorSaveItem";
+            this.screenRoomBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 23);
+            this.screenRoomBindingNavigatorSaveItem.Text = "Save Data";
+            this.screenRoomBindingNavigatorSaveItem.Click += new System.EventHandler(this.screenRoomBindingNavigatorSaveItem_Click);
+            // 
+            // screenRoomDataGridView
+            // 
+            this.screenRoomDataGridView.AutoGenerateColumns = false;
+            this.screenRoomDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.screenRoomDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.screenRoomDataGridView.DataSource = this.screenRoomBindingSource;
+            this.screenRoomDataGridView.Location = new System.Drawing.Point(35, 45);
+            this.screenRoomDataGridView.Name = "screenRoomDataGridView";
+            this.screenRoomDataGridView.RowHeadersWidth = 51;
+            this.screenRoomDataGridView.RowTemplate.Height = 24;
+            this.screenRoomDataGridView.Size = new System.Drawing.Size(652, 393);
+            this.screenRoomDataGridView.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "ScreenRoomId";
+            this.dataGridViewTextBoxColumn1.HeaderText = "ScreenRoomId";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            this.dataGridViewTextBoxColumn1.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "MovieId";
+            this.dataGridViewTextBoxColumn2.HeaderText = "MovieId";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "RoomNo";
+            this.dataGridViewTextBoxColumn3.HeaderText = "RoomNo";
+            this.dataGridViewTextBoxColumn3.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "TotalSeatNo";
+            this.dataGridViewTextBoxColumn4.HeaderText = "TotalSeatNo";
+            this.dataGridViewTextBoxColumn4.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.Width = 125;
+            // 
+            // ScreenRoomForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.screenRoomDataGridView);
+            this.Controls.Add(this.screenRoomBindingNavigator);
+            this.Name = "ScreenRoomForm";
+            this.Text = "ScreenRoomForm";
+            this.Load += new System.EventHandler(this.ScreenRoomForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.movieTheatreDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.screenRoomBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.screenRoomBindingNavigator)).EndInit();
+            this.screenRoomBindingNavigator.ResumeLayout(false);
+            this.screenRoomBindingNavigator.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.screenRoomDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MovieTheatreDataSet movieTheatreDataSet;
+        private System.Windows.Forms.BindingSource screenRoomBindingSource;
+        private MovieTheatreDataSetTableAdapters.ScreenRoomTableAdapter screenRoomTableAdapter;
+        private MovieTheatreDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingNavigator screenRoomBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton screenRoomBindingNavigatorSaveItem;
+        private System.Windows.Forms.DataGridView screenRoomDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+    }
+}
---
+++ MovieDatabaseFinal/ScreenRoomForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieDatabaseFinal
+{
+    public partial class ScreenRoomForm : Form
+    {
+        public ScreenRoomForm()
+        {
+            InitializeComponent();
+        }
+
+        private void screenRoomBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.screenRoomBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.movieTheatreDataSet);
+
+        }
+
+        private void ScreenRoomForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'movieTheatreDataSet.ScreenRoom' table. You can move, or remove it, as needed.
+            this.screenRoomTableAdapter.Fill(this.movieTheatreDataSet.ScreenRoom);
+
+        }
+    }
+}

4dfcbbf98a3318d8e85acb5851b7085c30a0ce1d
Munir Khaliqyar <le2176022@crc-lennox.qc.ca>
2022-11-23 08:58:08 -0500

    Create form named 'AccessMangerForm'

--- MovieDatabaseFinal/Form1.cs
+++ MovieDatabaseFinal/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace MovieDatabaseFinal
 {
-    public partial class Form1 : Form
+    public partial class AccessManagerForm : Form
     {
-        public Form1()
+        public AccessManagerForm()
         {
             InitializeComponent();
         }

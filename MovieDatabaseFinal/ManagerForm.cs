cca3d4f46f4893bc2304f2076a4090b37888022b
2125697 <LE2125697@crc-lennox.qc.ca>
2022-11-23 09:02:24 -0500

    Rename form 2 to managerForm

---
+++ MovieDatabaseFinal/ManagerForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieDatabaseFinal
+{
+    public partial class managerForm : Form
+    {
+        public managerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void clientButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

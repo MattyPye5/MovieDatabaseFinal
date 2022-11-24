ec67d72abbf2ea7ef51968ff50af6d8553144802
Munir Khaliqyar <le2176022@crc-lennox.qc.ca>
2022-11-24 11:16:22 -0500

    Make a database connection and code the "loginButton"

---
+++ MovieDatabaseFinal/AccessManagerPortalForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace MovieDatabaseFinal
+{
+    partial class AccessManagerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.loginButton.Location = new System.Drawing.Point(112, 260);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(127, 50);
+            this.loginButton.TabIndex = 0;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(84, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(82, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 63);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Access Manager portal";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.userNameTextBox.Location = new System.Drawing.Point(87, 129);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(183, 20);
+            this.userNameTextBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(84, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.passwordTextBox.Location = new System.Drawing.Point(87, 195);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(183, 20);
+            this.passwordTextBox.TabIndex = 5;
+            // 
+            // AccessManagerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 337);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.userNameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.loginButton);
+            this.Name = "AccessManagerForm";
+            this.Text = "Night At The Movies!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox passwordTextBox;
+    }
+}
+
---
+++ MovieDatabaseFinal/AccessManagerPortalForm.cs
@@ -0,0 +1,81 @@
+﻿using MovieDatabaseFinal.MovieTheatreDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieDatabaseFinal
+{
+    public partial class AccessManagerForm : Form
+    {
+        bool managerFound = false;
+        bool clientFound = false;
+        public AccessManagerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            string userName = userNameTextBox.Text;
+            string password = passwordTextBox.Text;
+
+            if (userName == "")
+            {
+                MessageBox.Show("Please enter your user name.");
+            } else if (password == "")
+            {
+                MessageBox.Show("Please enter your password.");
+            }
+            else
+            {
+                SqlConnection conn = new SqlConnection();
+                conn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\LE2176022\OneDrive - Champlain Regional College\Documents\Semester 3\OOP 2\FinalProject\MovieDatabaseFinal\MovieDatabaseFinal\MovieTheatre.mdf"";Integrated Security=True;Connect Timeout=30";
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "select * from UserData";
+                cmd.Connection = conn;
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    if (reader[1].ToString() == userName && reader[2].ToString() == password)
+                    {
+                        if (reader[3].ToString() == "Manager")
+                        {
+                            managerFound = true;
+                            break;
+                        }
+                        else if(reader[3].ToString() == "Client")
+                        {
+                            clientFound = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (managerFound)
+                {
+                    //ManagerForm myManagerForm = new ManagerForm;
+                    //myManagerForm.showDialogue();
+
+                }
+                else if (clientFound)
+                {
+  
+                }else
+                {
+                    MessageBox.Show("The data that you entered is incorrect.");
+                }
+            }
+        }
+    }
+}
